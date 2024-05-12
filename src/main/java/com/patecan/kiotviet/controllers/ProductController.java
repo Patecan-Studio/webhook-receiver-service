02b8e7b650f75a2e759a17fd541c08586915d2aa
Trần Thiên Trọng <tranthientrong.it@gmail.com>
2024-05-12 12:48:56 +0700

    Add receiver endpoint

--- src/main/java/com/patecan/kiotviet/controllers/ProductController.java
+++ src/main/java/com/patecan/kiotviet/controllers/ProductController.java
@@ -3,15 +3,20 @@ package com.patecan.kiotviet.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.*;
 
-@RestController("/webhooks/products")
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
     Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @GetMapping("/check")
+    public ResponseEntity<String> check() {
+        logger.info("check");
+        System.out.println("check");
+        return ResponseEntity.ok("check");
+    }
 
     @PostMapping("/update")
     public ResponseEntity<String> listenProductChange(@RequestBody String payload) {
